7bfb9bb196a2d068b511005994b48fbb6c2318b0
AbhiCodes <chhettriabhishek9@gmail.com>
2023-09-16 18:23:21 -0400

    Added tweet functionality

--- script.js
+++ script.js
@@ -10,3 +10,7 @@ const api_url = "https://api.quotable.io/random";
 }
 
 quotes(api_url);
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + author.innerHTML , "Tweet Window" , "width=600, height=300");
+}
